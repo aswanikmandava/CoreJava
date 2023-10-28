5ebbccbd26c6d8132441cea0998e6e6964bec3d0
Aswani K Mandava <aswani.k.mandava@gmail.com>
2023-10-28 18:42:31 -0400

    Added square brackets beside type for better readability

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
+    // use keyword "main" to autopopulate main method
     public static void main(String[] args) throws Exception {
+        // use "sysout" to print to output
         System.out.println("Hello, World!");
     }
 }
--- src/MyArray.java
+++ src/MyArray.java
@@ -1,8 +1,8 @@
 
 public class MyArray {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         // declare an int array
-        int my_arr[] = new int[3];
+        int[] my_arr = new int[3];
         // init some random values to it using for loop and Math.random method
         for (int i=0; i < my_arr.length; i++) {
             double val = Math.random()*100;
@@ -10,7 +10,7 @@ public class MyArray {
             System.out.println(my_arr[i]);
         }
         // declare a 2D array
-        int my_2d_arr[][] = new int[3][4];
+        int[][] my_2d_arr = new int[3][4];
         for (int i=0; i < my_2d_arr.length; i++) {
             for (int j=0; j < my_2d_arr[i].length; j++) {
                 double val = Math.random()*5;
@@ -22,7 +22,7 @@ public class MyArray {
         }
         System.out.println("Navigating 2D array in an efficient way");
         // efficient way of navigating 2D array
-        for (int num[]:my_2d_arr) {
+        for (int[] num:my_2d_arr) {
             for (int digit:num) {
                 System.out.print(digit + " ");
             }
@@ -31,7 +31,7 @@ public class MyArray {
         System.out.println("Initializing jagged array");
         // declare a 2D jagged array
         // size of inner array varies
-        int my_2d_j_arr[][] = new int[3][];
+        int[][] my_2d_j_arr = new int[3][];
         my_2d_j_arr[0] = new int[2];
         my_2d_j_arr[1] = new int[5];
         my_2d_j_arr[2] = new int[3];
@@ -46,7 +46,7 @@ public class MyArray {
         }
 
         System.out.println("Navigating jagged 2D array in an efficient way");
-        for (int num[]:my_2d_j_arr) {
+        for (int[] num:my_2d_j_arr) {
             for (int digit:num) {
                 System.out.print(digit + " ");
             }
